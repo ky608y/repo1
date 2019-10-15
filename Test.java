d0d600442550967409670b3885ee54ddcfd903ae
ky608y <kodandapani524@gmail.com>
2019-10-15 15:38:44 +0530

    New changes

--- Test.java
+++ Test.java
@@ -16,7 +16,7 @@ public class Test {
     while (iterator.hasNext()) {
 
       String s = iterator.next();
-     System.out.println("hello world...."+s);
+     System.out.println("hello world new...."+s);
     }
     System.out.println(map);
   }
